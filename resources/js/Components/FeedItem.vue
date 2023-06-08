cfc694c56c2d72c21401fbfa94b3a2521d1134f6
mazacon <mazacon@protonmail.com>
2023-06-07 20:28:54 -0500

    add FeedItem vue component

---
+++ resources/js/Components/FeedItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+
+const feed = defineProps([
+    'feeditem',
+]);
+
+</script>
+
+<template>
+    <div class="p-6 flex space-x-2">
+        <div class="flex-1">
+            {{ feeditem.title }} <br>
+            {{ feeditem.description }} <br>
+            <a :href="feeditem.link">{{ feeditem.link }}</a> <br>
+            {{ feeditem.pubDate }} <br><br>
+        </div>
+    </div>
+</template>
